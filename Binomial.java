1cdc164180e5a2046d658bf6d06256f54e382bb5
Orr Morag <orr.morag@post.runi.ac.il>
2025-12-28 12:26:09 +0200

    first 5 complete

--- Binomial.java
+++ Binomial.java
@@ -20,31 +20,23 @@ public class Binomial {
 	 }
 	
 	// Computes the Binomial function, efficiently
-	public static int binomial(int n, int k) {
-        if (k > n) return 0;
-        if (k == 0 || n == 0) return 1;
-        int[][] memo = new int[n + 1][k + 1];
-        for (int i = 0; i <= n; i++) {
-            Arrays.fill(memo[i], -1);
-        }
-        return binomial(n, k, memo);
+	public static long binomial(int n, int k) {
+		if (k > n) return 0L;
+		if (k == 0 || n == 0) return 1L;
+
+		long[][] memo = new long[n + 1][k + 1];
+		for (int i = 0; i <= n; i++) {
+			Arrays.fill(memo[i], -1L);
+		}
+		return binomial(n, k, memo);
 	}
 
-	private static int binomial(int n, int k, int[][] memo) {
-		if (memo[n][k] != -1) {
-			return memo[n][k];
-		}
-		// Base case
-		if ((k > n)) {
-		   	memo[n][k] = 0; 
-		   	return 0;
-		}
-		// Another base case
-		if (n == 0 || k == 0) {
-		   	memo[n][k] = 1; 
-		   	return 1;
-		}
-		memo[n][k] = binomial(n - 1, k, memo) + binomial(n - 1, k - 1, memo);
-		return memo[n][k];
+	private static long binomial(int n, int k, long[][] memo) {
+	if (memo[n][k] != -1L) return memo[n][k];
+
+    if (k > n) return memo[n][k] = 0L;
+    if (n == 0 || k == 0) return memo[n][k] = 1L;
+
+    return memo[n][k] = binomial(n - 1, k, memo) + binomial(n - 1, k - 1, memo);
 	}
 }
